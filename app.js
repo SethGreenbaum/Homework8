bfac280a6971a901bb1e56f31713845c327aabf6
Seth Greenbaum <sethmgreenbaum@gmail.com>
2020-02-29 14:11:07 -0700

    added inquirer and fs methods into main Team class

--- app.js
+++ app.js
@@ -4,32 +4,159 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
-const OUTPUT_DIR = path.resolve(__dirname, "output")
-const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
 const render = require("./lib/htmlRenderer");
-​
-​
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-​
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-​
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-​
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-​
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
+class Team {
+    // Save a reference for `this` in `this` as `this` will change inside of inquirer
+    constructor() {
+      this.teamSize = 0;
+      this.team =[];
+    }
+    start(){
+      if (this.teamSize ===0 ){
+        this.askForManager()
+      } else {this.askForNext()}
+    }
+    askForManager() {
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              name: "name",
+              message: "What is your manager's name?"
+            },
+            {
+              type: "input",
+              name: "id",
+              message: "What is your manager's id?"
+            },
+            {
+              type: "input",
+              name: "email",
+              message: "What is your manager's email?"
+            },
+            {
+              type: "input",
+              name: "officeNumber",
+              message: "What is your manager's officeNumber?"
+            }
+          ])
+          .then(val => {
+            const manager = new Manager(val.name, val.id, val.email, val.officeNumber);
+            this.teamSize += 1;
+            this.team.push(manager);
+            this.askForNext()
+          });
+    }
+    askForEngineer() {
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              name: "name",
+              message: "What is your engineer's name?"
+            },
+            {
+              type: "input",
+              name: "id",
+              message: "What is your engineer's id?"
+            },
+            {
+              type: "input",
+              name: "email",
+              message: "What is your engineer's email?"
+            },
+            {
+              type: "input",
+              name: "github",
+              message: "What is your engineer's github?"
+            }
+          ])
+          .then(val => {
+            const engineer = new Engineer(val.name, val.id, val.email, val.github);
+            this.teamSize += 1;
+            this.team.push(engineer)
+            this.askForNext()
+          });
+    }
+    askForIntern() {
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              name: "name",
+              message: "What is your intern's name?"
+            },
+            {
+              type: "input",
+              name: "id",
+              message: "What is your intern's id?"
+            },
+            {
+              type: "input",
+              name: "email",
+              message: "What is your intern's email?"
+            },
+            {
+              type: "input",
+              name: "school",
+              message: "What is your intern's school?"
+            }
+          ])
+          .then(val => {
+            const intern = new Intern(val.name, val.id, val.email, val.school);
+            this.teamSize += 1;
+            this.team.push(intern)
+            this.askForNext()
+          });
+    };
+    askForNext() {
+        inquirer
+            .prompt([
+                {
+                    type: "confirm",
+                    name: "moreTeam",
+                    message: "Are you adding more members to your team?"
+                }
+            ]).then( val => {
+                if (val.moreTeam===true){
+                    this.askType()
+                } else {this.renderTime()}
+            }   
+        )
+    };
+    askType(){
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "type",
+                message: "Are you adding an Intern or an Engineer to your team?"
+            },
+        ]).then(val => {
+            if (val.type==="intern"){
+                this.askForIntern()
+            } else if (val.type==="engineer") {
+                this.askForEngineer()
+            } else {this.askForNext()}
+         }
+        );
+    };
+    renderTime(){
+        fs.writeFile(outputPath, render(this.team), function(err) {
+
+            if (err) {
+              return console.log(err);
+            }
+          
+            console.log("Success!");
+          
+          });
+    };
+};
+
+var team = new Team();
+
+team.start();
